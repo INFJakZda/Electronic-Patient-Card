d8cd39b7f37568914d72585506477104ef2d9acf
eaglx <tango_delta@outlook.com>
2018-05-29 17:18:58 +0200

    Add code in Net, get list of patients

---
+++ IwMproject/FHIRconn.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+using Hl7.Fhir.Serialization;
+using Hl7.FhirPath;
+
+namespace IwMproject
+{
+    class FHIRconn
+    {
+        private string FhirClientEndPoint = "http://hapi.fhir.org/baseDstu3/";
+        private FhirClient client;
+
+        public FHIRconn()
+        {
+            client = new FhirClient(FhirClientEndPoint);
+        }
+
+        public List<Patient> GetListOfPatients(string patientName)
+        {
+            Patient patient;
+            List<Patient> patients = new List<Patient>();
+            var b = client.Search<Hl7.Fhir.Model.Patient>(new string[] { "name=" + patientName });
+
+            foreach (var Entry in b.Entry)
+            {
+                var x = Entry.Resource;
+                patient = (Patient)x;
+                patients.Add(patient);
+            }
+            return patients;
+        }
+
+        public List<Patient> GetListOfAllPatients()
+        {
+            List<Patient> patients = new List<Patient>();
+            for (char letter = 'A'; letter <= 'Z'; letter++)
+            {
+                patients.AddRange(GetListOfPatients(letter.ToString()));
+            }
+            return patients;
+        }
+    }
+}
---
+++ IwMproject/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+using Hl7.Fhir.Serialization;
+using Hl7.FhirPath;
+
+namespace IwMproject
+{
+    public partial class Form1 : Form
+    {
+        private FHIRconn connDB;
+
+        public Form1()
+        {
+            InitializeComponent();
+            connDB = new FHIRconn();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           if(String.IsNullOrEmpty(textBox1.Text))
+            {
+                List<Patient> patients = connDB.GetListOfAllPatients();
+
+                foreach (var l in patients)
+                {
+                    int numberPatient = 0;
+                    string name;
+                    try
+                    {
+                        name = l.Name[0].GivenElement[0].ToString();
+                    }
+                    catch
+                    {
+                        name = null;
+                    }
+
+                    if (!String.IsNullOrEmpty(name))
+                    {
+                        string outS = numberPatient + ". " + l.Id + " \t " + l.Name[0].GivenElement[0] + " \t " + l.Name[0].Family;
+                        listBox1.Items.Add(outS);
+                        numberPatient += 1;
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
